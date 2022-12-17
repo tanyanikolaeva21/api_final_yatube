68357ebc1a65d9bfae413c1db23f381b9ca45a0e
Татьяна Николаева <tanya8621@mail.ru>
2022-12-17 12:31:13 +0300

    15

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets, filters, status
 from rest_framework.response import Response
-from posts.models import Group, Post, User, Follow
+from posts.models import Group, Post, Follow
 from .permissions import IsAuthorOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import (
@@ -55,7 +55,7 @@ class FollowViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = FollowSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields =['=user__username', '=following__username']
+    search_fields = ['=user__username', '=following__username']
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
